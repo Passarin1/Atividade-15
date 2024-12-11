c9ce4fc6eb57b4bdd5e369111b44ba6b692c2a96
Luís Henrique <165834056+Passarin1@users.noreply.github.com>
2024-12-11 01:04:38 +0000

    Add - Nome

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
-  console.log("Rota GET/ solicitada");
-  res.json({
-    nome: "luis henrique",      // Substitua pelo seu nome
+//index.js
+app.get("/usuario/:id", async (req, res) => {
+    console.log("Rota GET /usuario/# solicitada");
+    try {
+      const usuario = await selectUsuario(req.params.id);
+      if (usuario.length > 0) res.json(usuario);
+      else res.status(404).json({ message: "Usuário não encontrado!" });
+    } catch (error) {
+      res.status(error.status || 500).json({ message: error.message || "Erro!" });
+    }
   });
-});
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
